7698abe6fb7579b3df462bb9d37385f8f428a660
MerelT <79907840+MerelT@users.noreply.github.com>
2021-04-21 16:18:05 +0200

    final version

--- incoming2source.m
+++ incoming2source.m
@@ -1,4 +1,6 @@
-% Phase 1 (organize the recorded files)
+% SCRIPT TO EXPORT DATA FROM INCOMING FILES TO SOURCE FILES
+%
+%Phase 1 (organize the recorded files)
 % - Collect all files from all devices
 % - Convert files in proprietary formats to open formats (oxy4->oxy3, mvn->mvnx)
 % - Scan the paper lab notes and questionnaires into a pdf file
@@ -6,15 +8,18 @@
 % - Place all files in directory organization following BIDS:
 % incoming2source.m
 %
-% ==> the source_standard data gets archived in the DAC and the source_private get archived in the Ceph storage at CNCZ?
+% ==> the source_standard data gets archived on the harddisk (Exp_ME), the
+%     shared mbneufy3 folder and the donders repository
+% the source_private get archived only on the harddisk (Exp_ME)
 %
+%?? Also for our experiment??
 % Phase 2a (add the metadata files): source2raw.m
 % - Extract the timing of the events from each individual recording file as trigger-events.tsv according to BIDS
 % - Determine the relative timing of all recordings  to each other and store as scans.tsv according to BIDS
 % - Determine the timing of the experimental runs/blocks/trials in each of the recordings
 %
 % Phase 2b (minimal pre-processing)
-% - Annotate the video data (freezing of gate), add it to events.tsv
+% - Annotate the video data (automaticity), add it to events.tsv
 % - Determine optode positions from 3D structure sensor scans
 % - Remove all files with identifying information (video, 3D scans)
 %
@@ -29,18 +34,19 @@
 
 
 %% Set parameters
+% adjust these for every subject!
 clear all
-root_dir = 'C:\Users\helen\Documents\freezing_fnirs\data';
-sub='sub-PD15';
+root_dir = 'F:\Experiment_ME\Data';
+sub='sub-06'; %indicate the subject ID
 cd(root_dir)
 
 rec_video=true;
-rec_nirs=[2];
+rec_nirs=[1]; %how many recordings where made? check for the correct one
 nirs_offline=true;
-rec_motion=[1 1 1 1];
+rec_motion=[1 1 1 1]; %all xsens data recorded? (footauto, footautodual, footnonauto, footnonautodual)
 take_motion=[1:4]; %of all recordings
-run_motion=[1:4];
-rec_stim=[1];
+task_motion=  {'footautodual', 'footauto', 'footnonauto', 'footnonautodual'}; %the order depends on the randomization 
+rec_stim=[1]; %how many recording where made? check for the correct one
 
 %% create source directory structure
 % source_standard
@@ -61,21 +67,18 @@ end
 if rec_video
 video_incom=fullfile(root_dir, 'incoming', sub, 'video');
 video_outgo= fullfile(root_dir, 'source_private', sub, 'video');
-% loop through all cam folders
-acq='acq-begin';
-files=dir(fullfile(video_incom, acq, '*.MP4'));
+files=dir(fullfile(video_incom,'*.MP4'));
     for j=1:length(files)
-        filename_n=sprintf('%s_automaticity_%s_rec-%.2d_video.mp4', sub,  acq, j);
+        filename_n=sprintf('%s_task-automaticity_rec-%.2d_video.mp4', sub, j);
         [success, message]=movefile(fullfile(files(j).folder, files(j).name),fullfile(video_outgo, filename_n));
         if success
             fprintf('File successfully moved to source directory: %s \n', filename_n);
         end
     end
 
-
 % all files moved to source directory?
 files=dir(video_incom);
-if length(files)>1 %contains directory ., .., acq-mobile, acq-end, acq-begin
+if length(files)>2 %contains directory ., .., acq-mobile, acq-end, acq-begin
     warning('Remaining files in incoming directory')
 else
     fprintf('No remaining files in incoming directory \n\n');
@@ -106,7 +109,7 @@ end
 for i=1:length(rec_nirs)
     for e=1:length(ext)
         filename_o=sprintf('%s_rec-%.2d_nirs.%s', sub, rec_nirs(i), ext{e});
-        filename_n=sprintf('%s_automaticity_acq-online_rec-%.2d_nirs.%s', sub, rec_nirs(i), ext{e});
+        filename_n=sprintf('%s_task-automaticity_acq-online_rec-%.2d_nirs.%s', sub, rec_nirs(i), ext{e});
         file=dir(fullfile(nirs_incom, filename_o));
         if length(file)==1
             [success, message]=movefile(fullfile(file.folder, file.name), fullfile(nirs_outgo, filename_n));
@@ -123,13 +126,13 @@ end
 if nirs_offline
   acq={'acq-24065', 'acq-24068'};
   for a=1:2
-    files=dir(fullfile(nirs_incom, acq{a}, '*.oxy3'));
+      files=dir(fullfile(nirs_incom, acq{a}, '*.oxy3'));
     if length(rec_nirs)~=length(files)
       error('Not the same number of offline files as expected')
     end
     for i=1:length(rec_nirs)
       %         filename_o=sprintf('%s_%s_rec-%.2d_nirs.oxy3', sub, acq{a}, rec_nirs(i));
-      filename_n=sprintf('%s_automaticity_%s_rec-%.2d_nirs.oxy3', sub, acq{a}, rec_nirs(i));
+      filename_n=sprintf('%s_task-automaticity_%s_rec-%.2d_nirs.oxy3', sub, acq{a}, rec_nirs(i));
       [success, message]=movefile(fullfile(files(i).folder, files(i).name), fullfile(nirs_outgo, filename_n));
       if success
         fprintf('File successfully moved to source directory: %s \n', filename_n);
@@ -151,7 +154,7 @@ end
 
 % all files moved to source directory?
 files=dir(fullfile(nirs_incom, '**/*.*'));
-if length(files)>6 %contains directory . and .. (x3); and acq-24065, acq-24068
+if length(files)>8 %contains directory . and .. (x3); and acq-24065, acq-24068
     warning(sprintf('Remaining files in incoming directory'))
 else
     fprintf('No remaining files in incoming directory \n\n');
@@ -179,7 +182,7 @@ end
 for i=1:length(rec_motion)
     for e=1:length(ext)
         filename_o=sprintf('%s_rec-%.2d_take-%.3d.%s',sub, rec_motion(i), take_motion(i), ext{e});
-        filename_n=sprintf('%s_automaticity_run-%.2d_motion.%s', sub, run_motion(i), ext{e});
+        filename_n=sprintf('%s_task-%s_motion.%s', sub, task_motion{i}, ext{e});
         file=dir(fullfile(motion_incom, filename_o));
         if length(file)==1
             [success, message]=movefile(fullfile(file.folder, file.name),fullfile(motion_outgo,filename_n));
@@ -205,27 +208,39 @@ end
 if rec_stim
 stim_incom=fullfile(root_dir, 'incoming', sub, 'stim');
 stim_outgo= fullfile(root_dir, 'source_standard', sub, 'stim');
-ext={'triggerinfo.mat', 'triggers.log', 'triggerslabrecorder.xdf'};
+ext={'triggers.log', 'triggerslabrecorder.xdf'};
 
-% move stimulation script
-m_script=dir(fullfile(stim_incom, sprintf('%s_script_freezing_fnirs*.m', sub)));
-if length(m_script)==1
-    [success, message]=movefile(fullfile(m_script(1).folder, m_script(1).name),fullfile(stim_outgo, m_script(1).name));
+% move AutomaticityTest script
+m_scripttest=dir(fullfile(stim_incom, sprintf('%s_AutomaticityTest_ME_v4.m', sub)));
+if length(m_scripttest)==1
+    [success, message]=movefile(fullfile(m_scripttest(1).folder, m_scripttest(1).name),fullfile(stim_outgo, m_scripttest(1).name));
     if success
-        fprintf('File successfully moved to source directory: %s \n', m_script(1).name);
+        fprintf('File successfully moved to source directory: %s \n', m_scripttest(1).name);
     end
 else
-    warning('No file named *_script_freezing_fnirs*.m in incoming directory or multiple files named like that');
+    warning('No file named *%s_AutomaticityTest_ME_v4.m* in incoming directory or multiple files named like that');
 end
 
-% move stim files of practice recording to source directory
-if ~strcmp(sub, 'sub-HC90') % practice recording was saved under PD61
-for e=1:length(ext)-1
-  filename_o=sprintf('%s_rec-practice_%s', sub, ext{e});
-  filename_n=sprintf('%s_task-gait_rec-practice_%s', sub, ext{e});
-  file=dir(fullfile(stim_incom,filename_o));
-  if length(file)==1
-    [success, message]=movefile(fullfile(file.folder, file.name), fullfile(stim_outgo, filename_n));
+% move Experiment script
+m_scriptexp=dir(fullfile(stim_incom, sprintf('%s_ExperimentScript_ME_v5.m', sub)));
+if length(m_scriptexp)==1
+    [success, message]=movefile(fullfile(m_scriptexp(1).folder, m_scriptexp(1).name),fullfile(stim_outgo, m_scriptexp(1).name));
+    if success
+        fprintf('File successfully moved to source directory: %s \n', m_scriptexp(1).name);
+    end
+else
+    warning('No file named *%s_ExperimentScript_ME_v5.m* in incoming directory or multiple files named like that');
+end
+
+% move events
+events={'events_footauto.mat', 'events_footautodual.mat', 'events_footnonauto.mat', 'events_footnonautodual.mat', 'events_handauto.mat', 'events_handautodual.mat', 'events_handnonauto.mat', 'events_handnonautodual.mat'};
+task={'footauto', 'footautodual', 'footnonauto', 'footnonautodual', 'handauto', 'handautodual', 'handnonauto', 'handnonautodual'};
+for i=1:8
+files=dir(fullfile(stim_incom, events{i}));
+filename_o=(events{i});
+filename_n=sprintf('%s_task-%s_events.mat', sub, task{i});
+  if length(files)==1
+    [success, message]=movefile(fullfile(files.folder, files.name), fullfile(stim_outgo, filename_n));
     if success
       fprintf('File successfully moved to source directory: %s \n', filename_n);
     end
@@ -239,7 +254,7 @@ end
 for i=1:length(rec_stim)
     for e=1:length(ext)
         filename_o=sprintf('%s_rec-%.3d_%s', sub, rec_stim(i), ext{e});
-        filename_n=sprintf('%s_task-gait_rec-%.2d_%s', sub, rec_stim(i), ext{e});
+        filename_n=sprintf('%s_task-automaticity_rec-%.2d_%s', sub, rec_stim(i), ext{e});
         file=dir(fullfile(stim_incom,filename_o));
         if length(file)==1
             [success, message]=movefile(fullfile(file.folder, file.name), fullfile(stim_outgo, filename_n));
@@ -258,7 +273,6 @@ if length(files)>2 %contains directory . and ..
     warning('Remaining files in incoming directory')
 else
     fprintf('No remaining files in incoming directory \n\n');
-end
 end
 
 %% structure sensor
@@ -313,7 +327,7 @@ end
 
 % all files moved to source directory?
 files=dir(fullfile(labn_incom));
-if length(files)>1 %contains directory . and ..
+if length(files)>2 %contains directory . and ..
     warning('Remaining files in incoming directory')
 else
     fprintf('No remaining files in incoming directory \n\n');
