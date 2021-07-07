1a8fdb0b7e632969f29fdce663b6d641c2177f4c
MerelT <79907840+MerelT@users.noreply.github.com>
2021-07-07 15:38:19 +0200

    updated version, statistical analysis part complete

--- multisubject_averaging.m
+++ multisubject_averaging.m
@@ -4,6 +4,7 @@ root_dir = 'C:\Users\mtabo\Documents\Data_Internship';
 
 %Indicate the subjects
 subjects = {'sub-06', 'sub-95', 'sub-53', 'sub-43', 'sub-19', 'sub-31', 'sub-38', 'sub-63', 'sub-90', 'sub-21', 'sub-44', 'sub-74', 'sub-84', 'sub-77', 'sub-69', 'sub-67', 'sub-27', 'sub-88'};
+%subjects = {'sub-06', 'sub-19'}
 
 %Indicate and open layout
 load(fullfile(root_dir, 'layout.mat')); % load layout
@@ -15,9 +16,13 @@ ft_layoutplot(cfg);
 clear data_all 
 for s = 1:length(subjects)
     for task=1:4
-        load(fullfile(root_dir, 'processedshortchanreg', subjects{s}, 'data_TL_blc'));
+        load(fullfile(root_dir, 'processedshort', subjects{s}, 'data_TL_blc'));
         data_all{task}{s}=data_TL_blc{task};
     end 
+end
+
+ for task=1:4
+     data_all_new{task}=data_all{task}';
  end
  
 %% 3. Average over all subjects -> for each condition seperately!
@@ -58,14 +63,37 @@ cfg                   = [];
 cfg.showlabels        = 'yes';
 cfg.layout            = layout;
 cfg.interactive       = 'yes'; % this allows to select a subplot and interact with it
-cfg.linecolor        = 'rbrbmcmc'; % O2Hb is showed in red (finger) and magenta (foot), HHb in blue (finger) and cyan (foot)
-cfg.linestyle = {'--', '--', '-', '-', ':', ':', '-.', '-.'}; % fingerauto is dashed line, fingernonauto is solid line, footauto is dotted line and footnonauto is a dotted stars line
-cfg.comment = 'fingerauto is dashed line, fingernonauto is solid line, footauto is dotted line and footnonauto is a dashed dot line';
-%cfg.ylim = [-0.440 0.540];
+%cfg.linecolor        = 'rbrbmcmc'; % O2Hb is showed in red (finger) and magenta (foot), HHb in blue (finger) and cyan (foot)
+cfg.linecolor        = 'rbmc';
+%cfg.linestyle = {'--', '--', '-', '-', ':', ':', '-.', '-.'}; % fingerauto is dashed line, fingernonauto is solid line, footauto is dotted line and footnonauto is a dotted stars line
+%cfg.comment = 'fingerauto is dashed line, fingernonauto is solid line, footauto is dotted line and footnonauto is a dashed dot line';
+cfg.linestyle = {'-', '-', '--', '--'};
+cfg.ylim = [-0.135 0.165];
 figure;
-ft_multiplotER(cfg, data_TL_O2Hb{1}, data_TL_HHb{1}, data_TL_O2Hb{2}, data_TL_HHb{2}, data_TL_O2Hb{3}, data_TL_HHb{3}, data_TL_O2Hb{4}, data_TL_HHb{4});
+%ft_multiplotER(cfg, data_TL_O2Hb{1}, data_TL_HHb{1}, data_TL_O2Hb{2}, data_TL_HHb{2}, data_TL_O2Hb{3}, data_TL_HHb{3}, data_TL_O2Hb{4}, data_TL_HHb{4});
+ft_multiplotER(cfg, data_TL_O2Hb{1}, data_TL_HHb{1}, data_TL_O2Hb{3}, data_TL_HHb{3});
 
 % g) Plot for each task seperately
+taskname={'Finger Auto', 'Finger Nonauto', 'Foot Auto', 'Foot Nonauto'};
+%taskshort={'complex', 'stroop'};
+for task=1:4
+    cfg                   = [];
+    cfg.showlabels        = 'yes';
+    cfg.layout            = layout;
+    cfg.showoutline       = 'yes';
+    cfg.interactive       = 'yes'; % this allows to select a subplot and interact with it
+    cfg.linecolor         = 'rb';% O2Hb is showed in red, HHb in blue
+    cfg.ylim = [-0.135 0.165];
+%     cfg.colorgroups=contains(data_TL_blc{task}.label, '[O2Hb]')+2*contains(data_TL_blc{task}.label, '[HHb]');
+    figure; 
+    title(taskname{task}); 
+    ft_multiplotER(cfg, data_TL_O2Hb{task}, data_TL_HHb{task})
+%     saveas(gcf, [char(taskshort(task)) '_timelock.jpg']);
+end
+
+
+%%
+% Plot for each task seperately
 taskname={'Finger Auto', 'Finger Nonauto', 'Foot Auto', 'Foot Nonauto'};
 %taskshort={'complex', 'stroop'};
 for task=1:4
@@ -79,7 +107,7 @@ for task=1:4
 %     cfg.colorgroups=contains(data_TL_blc{task}.label, '[O2Hb]')+2*contains(data_TL_blc{task}.label, '[HHb]');
     figure; 
     title(taskname{task}); 
-    ft_multiplotER(cfg, data_TL_O2Hb{task}, data_TL_HHb{task})
+    ft_multiplotER(cfg, data_lPPC_O2Hb{task}, data_lPPC_HHb{task})
 %     saveas(gcf, [char(taskshort(task)) '_timelock.jpg']);
 end
 
@@ -89,14 +117,14 @@ con_names={'finger auto',  'finger nonauto', 'foot auto', 'foot nonauto'};
 for i=1:4 % loop over the 4 conditions
   [stat_O2Hb, stat_HHb] = statistics_withinsubjects(grandavg{i}, 'grandavg', layout, i, con_names{i});
 end
-%% 6. Statistical analysis (not finished yet! still trying things out) 
+%% 6. Statistical analysis (paired t-test) 
 
-
-% T-TEST
+% T-TEST (determine p-value for single channels)
 % define the parameters for the statistical comparison
 cfg = [];
-cfg.channel     = 'Rx1-Tx2 [HHb]', 'Rx1-Tx2 [O2Hb]';
-cfg.latency     = 'all';
+cfg.channel     = {'Rx2-Tx3 [O2Hb]', 'Rx2-Tx4 [O2Hb]', 'Rx4-Tx4 [O2Hb]', 'Rx4-Tx3 [O2Hb]'};
+cfg.avgoverchan = 'yes';
+cfg.latency     = [5 10];
 cfg.avgovertime = 'yes';
 cfg.parameter   = 'avg';
 cfg.method      = 'analytic';
@@ -104,66 +132,17 @@ cfg.statistic   = 'ft_statfun_depsamplesT';
 cfg.alpha       = 0.05;
 cfg.correctm    = 'no';
 
-Nsub = 17;
+Nsub = 18;
 cfg.design(1,1:2*Nsub)  = [ones(1,Nsub) 2*ones(1,Nsub)];
 cfg.design(2,1:2*Nsub)  = [1:Nsub 1:Nsub];
 cfg.ivar                = 1; % the 1st row in cfg.design contains the independent variable
 cfg.uvar                = 2; % the 2nd row in cfg.design contains the subject number
 
-stat = ft_timelockstatistics(cfg, grandavg{1}(:), grandavg{2}(:));   % don't forget the {:}!
+% make sure to indicate the right tasks for which you want to determine if
+% the difference is significantly different
+stat = ft_timelockstatistics(cfg, data_all_new{1}{:}, data_all_new{2}{:})   % don't forget the {:}!
+stat_2 = ft_timelockstatistics(cfg, data_all_new{3}{:}, data_all_new{4}{:})
 
-% t-test with matlab function
-chan = 7;
-time = [-10 20];
-
-% find the time points for the effect of interest in the grand average data
-timesel_fingerauto = find(grandavg{1}.time >= time(1) & grandavg{1}.time <= time(2));
-timesel_fingernonauto  = find(grandavg{2}.time >= time(1) & grandavg{2}.time <= time(2));
-timesel_footauto = find(grandavg{3}.time >= time(1) & grandavg{3}.time <= time(2));
-timesel_footnonauto  = find(grandavg{4}.time >= time(1) & grandavg{4}.time <= time(2));
-
-% select the individual subject data from the time points and calculate the mean
-for isub = 1:17
-    valuesFA(isub) = mean(data_all{1}{isub}.avg(chan,timesel_fingerauto));
-    valuesFNA(isub)  = mean(data_all{2}{isub}.avg(chan,timesel_fingernonauto));
-    valuesFOA(isub) = mean(data_all{3}{isub}.avg(chan,timesel_fingerauto));
-    valuesFONA(isub)  = mean(data_all{4}{isub}.avg(chan,timesel_fingernonauto)); 
-end
-
-% plot to see the effect in each subject
-M = [valuesFA(:) valuesFNA(:)];
-figure; plot(M', 'o-'); xlim([0.5 2.5])
-%legend({'subj1', 'subj2', 'subj3', 'subj4', 'subj5', 'subj6', ...
-        %'subj7', 'subj8', 'subj9', 'subj10'}, 'location', 'EastOutside');
-
-FAminFNA = valuesFA - valuesFNA;
-FOAminFONA = valuesFOA - valuesFONA;
-FAminFOA = valuesFA - valuesFOA;
-[h,p,ci,stats] = ttest(FAminFNA, 0, 0.05) % H0: mean = 0, alpha 0.05
-[h,p,ci,stats] = ttest(FOAminFONA, 0, 0.05)
-[h,p,ci,stats] = ttest(FAminFOA, 0, 0.05)
-
-
-%loop over channels
-time = [-10 20];
-timesel_fingerauto = find(grandavg{1}.time >= time(1) & grandavg{1}.time <= time(2));
-timesel_fingernonauto  = find(grandavg{2}.time >= time(1) & grandavg{2}.time <= time(2));
-timesel_footauto = find(grandavg{3}.time >= time(1) & grandavg{3}.time <= time(2));
-timesel_footnonauto = find(grandavg{4}.time >= time(1) & grandavg{4}.time <= time(2));
-clear h p
-
-%FAminFNA = zeros(1,17);
-FAminFOA = zeros(1,17);
-
-for iChan = 1:8
-    for isub = 1:17
-        FAminFOA(isub) = ...
-            mean(data_all{1}{isub}.avg(iChan,timesel_fingerauto)) - ...
-            mean(data_all{3}{isub}.avg(iChan,timesel_footauto));
-    end
-
-    [h(iChan), p(iChan)] = ttest(FAminFOA, 0, 0.05 ) % test each channel separately
-end
 
 
 
